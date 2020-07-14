930051bda23f32eba569b0611543a3b18edd9cf2
David Bullen <david.bullen@gmail.com>
2020-07-14 19:08:31 +1200

    added basic move

--- src/app/views/network-builder-view/network-builder-view.component.ts
+++ src/app/views/network-builder-view/network-builder-view.component.ts
@@ -14,6 +14,9 @@ export class NetworkBuilderViewComponent implements OnInit {
   }
 
   shapesToDraw: Shape[] = [];
+  selectedShape : Shape;
+  startX = 0;
+  startY = 0;
 
   //Add Element
   addElement(type: string) {
@@ -21,4 +24,42 @@ export class NetworkBuilderViewComponent implements OnInit {
     this.shapeService.addShape(type);
     this.shapesToDraw = this.shapeService.getShapes(); 
   }     
+
+  //Adjust
+  startDrawing(evt: MouseEvent) { 
+    //Check if inside a shape then this is adjusting
+    this.checkIfPointIsInAnyShape(evt.offsetX, evt.offsetY)
+  }
+
+  keepDrawing(evt: MouseEvent) {
+    let deltaX = evt.x - this.startX;
+    let deltaY = evt.y - this.startY;
+    if (this.selectedShape){
+      this.selectedShape.x += deltaX;
+      this.selectedShape.y += deltaY;
+    }
+    this.startX = evt.x;
+    this.startY = evt.y;
+  }
+
+  stopDrawing(evt: MouseEvent) {
+    this.selectedShape = null;
+    this.startX = 0;
+    this.startY = 0;
+  }
+
+  checkIfPointIsInAnyShape(x: number, y: number){
+    for (let thisShape of this.shapesToDraw) {
+      console.log (thisShape.x);
+      if (x > thisShape.x 
+      && x < thisShape.x + thisShape.w
+      && y > thisShape.y
+      && y < thisShape.y + thisShape.h) {
+        //set a non-null createdShape to indicate we are adjusting
+        this.selectedShape = thisShape;
+        this.startX = x;
+        this.startY = y;
+      }
+    }    
+  }  
 }
